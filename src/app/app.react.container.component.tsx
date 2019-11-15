fed6760230ded433d91c45c8955f46330fe8bd1d
Nadav42 <bscpkd@Gmail.com>
2019-11-15 17:38:36 +0200

    - added support for multiple react instances in same angular page - switched react imports to work in angular production build

--- src/app/app.react.container.component.tsx
+++ src/app/app.react.container.component.tsx
@@ -1,24 +1,48 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
 
-import * as React from "react"
-import * as ReactDOM from 'react-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 import Button from './react/button';
 
-let reactContainerId = "react-demo-app"
+let reactContainerId = "react-buton-demo"
+let GENERATED_SEQUENCE_ID = 1;
 
 @Component({
-  selector: 'react-app',
-  template: `<div id="${reactContainerId}"></div>`
+	selector: 'react-button',
+	template: `<div id="${reactContainerId}-{{reactSequenceId}}"></div>`
 })
 export class ReactContainerComponent {
-  title = 'angular-react';
+	title = 'angular-react';
 
-  testClickCallback(data) {
-    console.log("angular recived click", data);
-  }
+	reactSequenceId: number;
 
-  ngAfterViewInit() {
-    ReactDOM.render(<Button startingValue={5} text={"Clicks: "} clickCallback={this.testClickCallback} />, document.querySelector(`#${reactContainerId}`))
-  }
+	constructor() {
+		this.reactSequenceId = GENERATED_SEQUENCE_ID;
+		GENERATED_SEQUENCE_ID = GENERATED_SEQUENCE_ID + 1;
+	}
+
+	testClickCallback(data) {
+		console.log("angular recived click", data);
+	}
+
+	loadReact() {
+		let reactContainerElement = document.querySelector(`#${reactContainerId}-${this.reactSequenceId}`);
+		console.log(reactContainerElement);
+
+		if (reactContainerElement) {
+			ReactDOM.render(<Button startingValue={5} text={"Clicks: "} clickCallback={this.testClickCallback} />, reactContainerElement)
+		}
+	}
+
+	ngOnChanges(changes: SimpleChanges) {
+		// if angular inputs change
+		this.loadReact();
+	}
+
+	ngAfterViewInit() {
+		this.loadReact();
+	}
 }
+
+
